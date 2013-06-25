162a8ad5e86781c17788e0b526d686f48565738f
David Shorten <dpshorten@gmail.com>
2013-06-25 17:21:14 +0200

    mutate() completed in Genotype.

--- HonoursProject/src/Genotype.java
+++ HonoursProject/src/Genotype.java
@@ -14,7 +14,8 @@ public class Genotype
 	 * 3 - West
 	 * 4 - Stay
 	 */
-	
+	private final int WEIGHT_MUTATIONS_PER_GENOTYPE_MUTATION = 1;
+	private final double MUTATION_AMOUNT_STDEV = 0.1;
 	private Vector<Double> inputWeights;
 	private Vector<Double> outputWeights;
 	
@@ -32,7 +33,18 @@ public class Genotype
 	}
 	
 	public void mutate() {
-		//stub
+		for(int i = 0; i < WEIGHT_MUTATIONS_PER_GENOTYPE_MUTATION; i++) {
+			Random random = new Random();
+			int indexToMutate = random.nextInt(inputWeights.size() + outputWeights.size());
+			double mutationAmount = random.nextGaussian() * MUTATION_AMOUNT_STDEV; 
+			if(indexToMutate < outputWeights.size()) {
+				indexToMutate %= outputWeights.size();
+				outputWeights.set(indexToMutate, outputWeights.get(indexToMutate).doubleValue() + mutationAmount);
+			} else {
+				indexToMutate %= inputWeights.size();
+				inputWeights.set(indexToMutate, inputWeights.get(indexToMutate).doubleValue() + mutationAmount);
+			}
+		}
 	}
 	
 	public static Genotype crossover(Genotype geno1, Genotype geno2) {
