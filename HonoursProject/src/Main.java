be1aaeccaccab4583a7a44dd504a7750a254ba7c
Schalk Jacobus Jordaan <schalkjordaan@gmail.com>
2013-08-05 21:43:34 +0200

    test

--- HonoursProject/src/Main.java
+++ HonoursProject/src/Main.java
@@ -4,11 +4,7 @@ public class Main
 {
 	public static void main (String [] args)
 	{
-<<<<<<< HEAD
 		final int boardSize = 10;
-=======
-		final int boardSize = 20;
->>>>>>> cdc6f22ccd94ab415006c85407ad255d035ac04a
 		final int numPrey = 1;
 		
 		Environment env = new Environment(boardSize, 1);
@@ -39,29 +35,18 @@ public class Main
 		RuleBasedBehaviour RBB1 = new RuleBasedBehaviour(boardSize);
 		
 		predatorPieces.add(new Piece(2,1,false,env,RBB1));
-<<<<<<< HEAD
 		predatorPieces.add(new Piece(2,2,false,env,RBB1));
 		predatorPieces.add(new Piece(2,3,false,env,RBB1));
 		
 		preyPieces.add(new Piece(0, 1, true, env, runAway));
-=======
-		predatorPieces.add(new Piece(11,11,false,env,RBB1));
-		predatorPieces.add(new Piece(3,3,false,env,RBB1));
-				
-		preyPieces.add(new Piece(15, 15, true, env, runAway));
->>>>>>> cdc6f22ccd94ab415006c85407ad255d035ac04a
 		
 		//for (int i = 0; i < numPrey; i++) {
 		//	preyPieces.add(new Piece(0, i, true, env, runAway));
 		//}
 		env.setPieces(predatorPieces, preyPieces);
-<<<<<<< HEAD
 		
 		SimulationResult result = env.run(true, false);
 		
-=======
-		SimulationResult result = env.run(true, true);
->>>>>>> cdc6f22ccd94ab415006c85407ad255d035ac04a
 		System.out.println("Board size: " + boardSize);
 		System.out.println(result.preyCaught);
 	}
